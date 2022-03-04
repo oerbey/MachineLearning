65591cef0252c8abd909ef901fb930e7517646d3
oerbey <66864050+oerbey@users.noreply.github.com>
2022-03-04 22:33:20 +0200

    ML prediction studies

---
+++ ML/2-Classification/Lesson1-LogisticReg/logisticreg.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("testdata.csv")
+#print(df.head())
+#print(df.isnull().sum().sum()) # sum of null values
+
+# 2.2 Missing Data Handling
+from sklearn.impute import SimpleImputer
+
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+
+x = df.iloc[:,1:4] # dependent variable
+y = df.iloc[:,4:] # independent variable
+
+x = pd.DataFrame(imputer.fit_transform(x))
+
+
+## Test split
+from sklearn.model_selection import train_test_split
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.33, random_state = 42)
+
+## Scaling
+from sklearn.preprocessing import StandardScaler
+
+sc = StandardScaler()
+
+xTrain = sc.fit_transform(x_train) # fit trains
+xTest = sc.transform(x_test) # transorm applies/uses the data
+
+from sklearn.linear_model import LogisticRegression
+logreg = LogisticRegression(random_state = 42)
+logreg.fit(xTrain, y_train.values.ravel())
+
+y_pred = logreg.predict(xTest)
+print(y_pred)
+print(y_test)
+
+## Confusion Matrix
+# this shows how our data is classified. First row is correct classification
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
