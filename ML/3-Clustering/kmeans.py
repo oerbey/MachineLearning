a6e5b87f0b7222242805af0566aa263c52e1fb10
oerbey <66864050+oerbey@users.noreply.github.com>
2022-04-17 11:30:40 +0300

    Hierarchical Clustering & Data Visualization

--- ML/3-Clustering/kmeans.py
+++ ML/3-Clustering/kmeans.py
@@ -8,6 +8,7 @@ df = pd.read_csv('customers.csv')
 
 X = df.iloc[:,2:4].values
 
+#K-Means
 from sklearn.cluster import KMeans
 
 kmeans = KMeans(n_clusters = 3, init = 'k-means++')
@@ -25,6 +26,16 @@ for i in range(1,11):
 
 print(results)
 
-kmeansfig = plt.figure(figsize = (11,7))
-plt.plot(range(1,11), results)
-plt.savefig("kmeansfig.png", dpi=300)
+# kmeansfig = plt.figure(figsize = (11,7))
+# plt.plot(range(1,11), results)
+# plt.savefig("kmeansfig.png", dpi=300)
+
+kmeans = KMeans(n_clusters = 3, init = 'k-means++', random_state = 42)
+Y_predict = kmeans.fit_predict(X)
+
+plt.scatter(X[Y_predict == 0, 0], X[Y_predict == 0, 1], s = 100, c = 'red')
+plt.scatter(X[Y_predict == 1, 0], X[Y_predict == 1, 1], s = 100, c = 'blue')
+plt.scatter(X[Y_predict == 2, 0], X[Y_predict == 2, 1], s = 100, c = 'green')
+plt.title("KMeans")
+kmeansplt = plt.show()
+plt.savefig("kmeansplt.png", dpi = 300)
